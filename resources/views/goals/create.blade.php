a59a94532b918c025f957d9cc1cfb09a1c28df3f
Alireza <Alirezabehnamnik@gmail.com>
2026-01-18 11:17:51 +0400

    remove the minimum validation in blade

--- resources/views/goals/create.blade.php
+++ resources/views/goals/create.blade.php
@@ -29,7 +29,7 @@
           <div class="col-span-3">
             <label for="goal" class="block text-sm/6 font-medium text-white">Goal</label>
             <div class="mt-2">
-              <input id="goal" type="number" min="1000" name="goal" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+              <input id="goal" type="number" name="goal" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
             </div>
           </div>
 
--- resources/views/savings/create.blade.php
+++ resources/views/savings/create.blade.php
@@ -22,7 +22,7 @@
           <div class="col-span-full">
             <label for="amount" class="block text-sm/6 font-medium text-white">Amount</label>
             <div class="mt-2">
-              <input id="amount" type="number" min="100" name="amount" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+              <input id="amount" type="number" name="amount" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
             </div>
           </div>
         </div>
